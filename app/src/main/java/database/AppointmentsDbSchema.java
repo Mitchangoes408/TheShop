96a61c7a84b272db09fc668d1030a05159bcedbf
Mitchangoes408 <54597899+Mitchangoes408@users.noreply.github.com>
2022-03-07 13:35:58 -0800

    Added AppointmentsDbSchema in case I decide to use a seperate appointment database

---
+++ app/src/main/java/database/AppointmentsDbSchema.java
@@ -0,0 +1,12 @@
+package database;
+
+public class AppointmentsDbSchema {
+    //table name
+    public static final String NAME = "appointments";
+
+    //columns
+    public static final class Cols {
+        public static final String TITLE = "title";
+        public static final String DATE = "date";
+    }
+}
